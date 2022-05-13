Update with fix for softmax

torch/nn/modules/activation.py
@@ -1304,9 +1304,10 @@ class Softmin(Module):
         self.dim = dim
 
     def __setstate__(self, state):
-        self.__dict__.update(state)
         if not hasattr(self, 'dim'):
             self.dim = None
+
+        super().__setstate__(state)
 
     def forward(self, input: Tensor) -> Tensor:
         return F.softmin(input, self.dim, _stacklevel=5)
@@ -1360,9 +1361,10 @@ class Softmax(Module):
         self.dim = dim
 
     def __setstate__(self, state):
-        self.__dict__.update(state)
         if not hasattr(self, 'dim'):
             self.dim = None
+
+        super().__setstate__(state)
 
     def forward(self, input: Tensor) -> Tensor:
         return F.softmax(input, self.dim, _stacklevel=5)
@@ -1431,9 +1433,10 @@ class LogSoftmax(Module):
         self.dim = dim
 
     def __setstate__(self, state):
-        self.__dict__.update(state)
         if not hasattr(self, 'dim'):
             self.dim = None
+
+        super().__setstate__(state)
 
     def forward(self, input: Tensor) -> Tensor:
         return F.log_softmax(input, self.dim, _stacklevel=5)